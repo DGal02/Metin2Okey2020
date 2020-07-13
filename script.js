51da128af5bb5694b708807b78d1d8743c89f7a7
DGal02 <dominik.galczyk@gmail.com>
2020-07-14 00:39:25 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function ($scope) {
+    $scope.red = [1, 2, 3, 4, 5, 6, 7, 8];
+    $scope.yellow = [1, 2, 3, 4, 5, 6, 7, 8];
+    $scope.blue = [1, 2, 3, 4, 5, 6, 7, 8];
+    $scope.cards = [
+        { color: 'red', numbers: [1, 2, 3, 4, 5, 6, 7, 8] },
+        { color: 'yellow', numbers: [1, 2, 3, 4, 5, 6, 7, 8] },
+        { color: 'blue', numbers: [1, 2, 3, 4, 5, 6, 7, 8] }
+
+    ]
+    $scope.myFunc = function (event) {
+        $scope.id = event.target.id;
+        $scope.number = $scope.id.charAt(0);
+        $scope.color = $scope.id.slice(1, $scope.id.length - 1)
+        $scope.toggle = $scope.id.charAt($scope.id.length - 1);
+
+        if ($scope.toggle == "0") {
+
+            document.getElementById($scope.id).src = "images/back.png";
+            document.getElementById($scope.id).id = $scope.number + $scope.color + 1;
+
+
+        } else {
+            document.getElementById($scope.id).src = "images/" + $scope.number + "_" + $scope.color + ".png";
+            document.getElementById($scope.id).id = $scope.number + $scope.color + 0;
+
+        }
+    };
+});
+
+$('#popoverhide').popover().click(function () {
+    setTimeout(function () {
+        $('#popoverhide').popover('hide');
+    }, 1500);
+});
+function restart() {
+    $(".img").each(function () {
+        let id = $(this).attr('id');
+        let number = id.charAt(0);
+        let color = id.slice(1, id.length - 1);
+        $(this).attr("src", "images/" + number + "_" + color + ".png");
+        $(this).attr("id", number + color + 0)
+    });
+};
