8f22652867cf45ad57bc27a53dc7b3b0d9677120
DGal02 <dominik.galczyk@gmail.com>
2020-07-15 21:15:11 +0200

    animation

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ var app = angular.module('myApp', []);
 function sleep(milliseconds) {
     var start = new Date().getTime();
     for (var i = 0; i < 1e7; i++) {
-      if ((new Date().getTime() - start) > milliseconds){
-        break;
-      }
+        if ((new Date().getTime() - start) > milliseconds) {
+            break;
+        }
     }
-  }
+}
 app.controller('myCtrl', function ($scope) {
     $scope.red = [1, 2, 3, 4, 5, 6, 7, 8];
     $scope.yellow = [1, 2, 3, 4, 5, 6, 7, 8];
@@ -17,26 +17,31 @@ app.controller('myCtrl', function ($scope) {
         { color: 'blue', numbers: [1, 2, 3, 4, 5, 6, 7, 8] }
 
     ]
-    $scope.myFunc = async function (event) {
-        $scope.id =  event.target.id;
+    $scope.myFunc = function (event) {
+        $scope.id = event.target.id;
         $scope.number = $scope.id.charAt(0);
         $scope.color = $scope.id.slice(1, $scope.id.length - 1)
         $scope.toggle = $scope.id.charAt($scope.id.length - 1);
 
+
         if ($scope.toggle == "0") {
-
-            $("#" + $scope.id).attr("src", "images/back.png");
-            document.getElementById($scope.id).id = $scope.number + $scope.color + 1;
+            $("#" + $scope.id).removeClass("fade2");
+            $("#" + $scope.id).addClass("fade");
             
-            
-            
-
+            setTimeout(function () {
+                $("#" + $scope.id).attr("src", "images/back.png");
+                document.getElementById($scope.id).id = $scope.number + $scope.color + 1;
+                
+            }, 125);
 
         } else {
-            $("#" + $scope.id).attr("src", "images/" + $scope.number + "_" + $scope.color + ".png");
-            // document.getElementById($scope.id).src = "images/" + $scope.number + "_" + $scope.color + ".png";
-            document.getElementById($scope.id).id = $scope.number + $scope.color + 0;
-
+            $("#" + $scope.id).removeClass("fade");
+            $("#" + $scope.id).addClass("fade2");
+            setTimeout(function () {
+                $("#" + $scope.id).attr("src", "images/" + $scope.number + "_" + $scope.color + ".png");
+                document.getElementById($scope.id).id = $scope.number + $scope.color + 0;
+              
+            }, 125);
         }
     };
 });
@@ -52,6 +57,8 @@ function restart() {
         let number = id.charAt(0);
         let color = id.slice(1, id.length - 1);
         $(this).attr("src", "images/" + number + "_" + color + ".png");
-        $(this).attr("id", number + color + 0)
+        $(this).attr("id", number + color + 0);
+        $(this).removeClass("fade2");
+        $(this).removeClass("fade");
     });
 };
