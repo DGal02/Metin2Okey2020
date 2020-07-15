d1b8967015647b0f5f3ebe99a948c746eaaad901
DGal02 <dominik.galczyk@gmail.com>
2020-07-15 22:48:51 +0200

    animation_fix

--- script.js
+++ script.js
@@ -46,19 +46,25 @@ app.controller('myCtrl', function ($scope) {
     };
 });
 
-$('#popoverhide').popover().click(function () {
-    setTimeout(function () {
-        $('#popoverhide').popover('hide');
-    }, 1500);
-});
+
 function restart() {
     $(".img").each(function () {
         let id = $(this).attr('id');
         let number = id.charAt(0);
         let color = id.slice(1, id.length - 1);
-        $(this).attr("src", "images/" + number + "_" + color + ".png");
-        $(this).attr("id", number + color + 0);
+        let toggle = id.charAt( id.length - 1);
         $(this).removeClass("fade2");
         $(this).removeClass("fade");
+        if (toggle == 1) {
+            $(this).animate({opacity:0}, 125);
+            setTimeout(() => {
+                $(this).attr("src", "images/" + number + "_" + color + ".png");
+            $(this).attr("id", number + color + 0);
+            }, 125);
+            $(this).animate({opacity:1}, 125);
+        }
+       
     });
 };
+
+
